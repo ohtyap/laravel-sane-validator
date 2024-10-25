6e972031899febd30ade7e25912e163f85840cc1
Thomas Payer <me@tpa.codes>
2024-10-25 18:31:02 +0200

    strategies to disable/enable sane validation

--- src/Validator.php
+++ src/Validator.php
@@ -8,18 +8,53 @@ use Illuminate\Validation\Validator as IlluminateValidator;
 
 class Validator extends IlluminateValidator
 {
+    private static bool $defaultEnableSaneValidation =  true;
+    private ?bool $enabledSaneValidation = null;
+
+    public static function defaultEnableSaneValidation(bool $defaultEnableSaneValidation): void
+    {
+        self::$defaultEnableSaneValidation = $defaultEnableSaneValidation;
+    }
+
+    public function enableSaneValidation(): void
+    {
+        $this->enabledSaneValidation = true;
+    }
+
+    public function disableSaneValidation(): void
+    {
+        $this->enabledSaneValidation = false;
+    }
+
+    private function isUsingSaneValidationBehavior(): bool
+    {
+        return ($this->enabledSaneValidation === true || self::$defaultEnableSaneValidation = true);
+    }
+
     protected function presentOrRuleIsImplicit($rule, $attribute, $value)
     {
+        if (!$this->isUsingSaneValidationBehavior()) {
+            return parent::presentOrRuleIsImplicit($rule, $attribute, $value);
+        }
+
         return $this->validatePresent($attribute, $value) || $this->isImplicit($rule);
     }
 
     public function validateRequired($attribute, $value)
     {
+        if (!$this->isUsingSaneValidationBehavior()) {
+            return parent::validateRequired($attribute, $value);
+        }
+
         return $this->validatePresent($attribute, $value);
     }
 
     public function validateFilled($attribute, $value)
     {
+        if (!$this->isUsingSaneValidationBehavior()) {
+            return parent::validateFilled($attribute, $value);
+        }
+
         if (!$this->validatePresent($attribute, $value)) {
             return true;
         }
